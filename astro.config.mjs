52d4cb915753fa4754ffd5132ba98aff12eacf14
aaronburt <email@aaronburt.co.uk>
2023-09-13 21:56:30 +0100

    Fixed commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,6 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-<<<<<<< HEAD
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-=======
 			title: 'Aaron',
 			social: {
 				github: 'https://github.com/aaronburt',
@@ -65,7 +47,6 @@ export default defineConfig({
 					autogenerate: { directory: 'reference' },
 				},
 				*/
->>>>>>> a09ed55 (Commit)
 			],
 		}),
 	],
