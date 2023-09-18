0ae44fd37a4f7f92a2e9b9fbda08bbc8c7d869c7
aaronburt <email@aaronburt.co.uk>
2023-09-18 04:10:03 +0100

    added favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
+			favicon: '/public/favicon.ico',
 			title: 'Aaron',
 			social: {
 				github: 'https://github.com/aaronburt',
